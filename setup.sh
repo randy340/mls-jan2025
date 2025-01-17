6217205d4ee375745c5e49fa5c862daed67cacee
randy340 <randy340@gmail.com>
2025-01-16 22:12:14 -0600

    Update setup.sh
    Add Azure hierarchy

--- setup.sh
+++ setup.sh
@@ -26,9 +26,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="testml"
-LOCATION="eastus"
-WORKSPACE="azureml"
+GROUP="GL-Oct2024"
+LOCATION="eastus2"
+WORKSPACE="GL-OCT2024"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
